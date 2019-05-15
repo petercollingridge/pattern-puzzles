ae23e46578512543f56df34cf89cb7cd02f86616
Peter Collingridge <peter.collingridge@gmail.com>
2019-05-15 22:57:54 +0100

    Start graph object

--- static/js/makeGraph.js
+++ static/js/makeGraph.js
@@ -1,5 +1,7 @@
-var puzzleArea = document.getElementById('puzzle');
 var NS = 'http://www.w3.org/2000/svg';
+var nodeRadius = 5;
+
+var puzzleArea = document.getElementById('puzzle');
 
 function addElement(type, parent, attributes) {
     var el = document.createElementNS(NS, type);
@@ -14,4 +16,9 @@ function addCircle(parent, attributes) {
     return addElement('circle', parent, attributes);
 }
 
-addCircle(puzzleArea, { r: 5, 'class': 'empty-region' });
+addCircle(puzzleArea, { r: nodeRadius, 'class': 'empty-region' });
+
+var graph = {
+    nodes: [[-1, 0], [1, 0]],
+    edges: [[0, 1]]
+};
