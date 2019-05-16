8379c767176bd8ece2a775a9af1651fa40c99687
Peter Collingridge <peter.collingridge@gmail.com>
2019-05-16 23:04:31 +0100

    Add ability to change selected colour

--- static/js/makePage.js
+++ static/js/makePage.js
@@ -1,3 +1,10 @@
+function getColourPickerFunction(i) {
+    return function() {
+        var puzzle = document.getElementById('puzzle');
+        puzzle.classList.add("colour-" + (i + 1));
+    };
+}
+
 function addColourPalette(parent, nColours) {
     var r = 16;
 
@@ -10,12 +17,16 @@ function addColourPalette(parent, nColours) {
     var svg = addSVGElement('svg', parent, paletteAttributes);
 
     for (var i = 0; i < nColours; i++) {
-        addCircle(svg, {
+        var attr = {
             cx: (2 + i * 4) * r,
             cy: 25,
             r: r,
             'class': "toolbar-selection colour-" + (i + 1)
-        });
+        };
+
+        var colourPicker = addCircle(svg, attr);
+        var clickFunc = getColourPickerFunction(i);
+        colourPicker.addEventListener('click', clickFunc);
     }
 
 }
