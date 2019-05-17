6b3094830cff4014df7c2d8510382af6d0f87fc2
Peter Collingridge <peter.collingridge@nanoporetech.com>
2019-05-17 22:36:17 +0100

    Remove unused code

--- static/js/makeGraph.js
+++ static/js/makeGraph.js
@@ -1,9 +1,4 @@
 
-var nodeRadius = 5;
-
-var puzzleArea = document.getElementById('puzzle');
-
-addColourable('circle', puzzleArea, { r: nodeRadius });
 
 var graph = {
     nodes: [[-1, 0], [1, 0]],
