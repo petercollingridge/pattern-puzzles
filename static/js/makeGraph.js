bd67723446da720e3bea86718bf318f87a6c9e65
Peter Collingridge <peter.collingridge@nanoporetech.com>
2019-05-17 13:00:23 +0100

    Create Puzzle object and get click to add colour working.

--- static/js/makeGraph.js
+++ static/js/makeGraph.js
@@ -3,7 +3,7 @@ var nodeRadius = 5;
 
 var puzzleArea = document.getElementById('puzzle');
 
-addCircle(puzzleArea, { r: nodeRadius, 'class': 'empty-region' });
+addColourable('circle', puzzleArea, { r: nodeRadius });
 
 var graph = {
     nodes: [[-1, 0], [1, 0]],
--- static/js/makePage.js
+++ static/js/makePage.js
@@ -1,36 +1,68 @@
-function getColourPickerFunction(i) {
-    return function() {
-        var puzzle = document.getElementById('puzzle');
-        puzzle.classList.add("colour-" + (i + 1));
-    };
-}
+var COLOURS = [
+    'rgb(20, 146, 196)',
+    'rgb(196, 20, 96)',
+];
 
-function addColourPalette(parent, nColours) {
-    var r = 16;
+var Puzzle = (function() {
+    var puzzleElement = document.getElementById('puzzle')
+    var toolbar = document.getElementById('toolbar');
 
-    var paletteAttributes = {
-        id: 'colour-palette',
-        height: '50px',
-        width: (nColours * r * 4) + 'px'
-    };
-
-    var svg = addSVGElement('svg', parent, paletteAttributes);
-
-    for (var i = 0; i < nColours; i++) {
-        var attr = {
-            cx: (2 + i * 4) * r,
-            cy: 25,
-            r: r,
-            'class': "toolbar-selection colour-" + (i + 1)
-        };
-
-        var colourPicker = addCircle(svg, attr);
-        var clickFunc = getColourPickerFunction(i);
-        colourPicker.addEventListener('click', clickFunc);
+    var Puzzle = {
+        selectedColour: false,
+        element: puzzleElement,
+        nodeRadius: 5
     }
 
-}
+    function getColourPickerFunction(i) {
+        return function() {
+            Puzzle.selectedColour = i + 1;
+        };
+    }
 
-var toolbar = document.getElementById('toolbar');
+    function addColour() {
+        this.setAttribute('class', "colour-" + Puzzle.selectedColour);
+    }
+    
+    Puzzle.createColourPalette = function(nColours) {
+        var r = 16;
+    
+        var paletteAttributes = {
+            id: 'colour-palette',
+            height: '50px',
+            width: (nColours * r * 4) + 'px'
+        };
+    
+        var svg = addSVGElement('svg', toolbar, paletteAttributes);
+    
+        for (var i = 0; i < nColours; i++) {
+            var attr = {
+                cx: (2 + i * 4) * r,
+                cy: 25,
+                r: r,
+                'class': "toolbar-selection colour-" + (i + 1)
+            };
+    
+            var colourPicker = addCircle(svg, attr);
+            var clickFunc = getColourPickerFunction(i);
+            colourPicker.addEventListener('click', getColourPickerFunction(i));
+        }
+    
+    };
 
-addColourPalette(toolbar, 2);
+    Puzzle.addColourable = function(type, attributes) {
+        attributes = attributes || {};
+        attributes.r = Puzzle.nodeRadius;
+        attributes.class = 'empty-region';
+
+        var el = addSVGElement(type, puzzleElement, attributes);
+        el.addEventListener('click', addColour);
+        return el;
+    };
+
+    return Puzzle;
+})();
+
+
+
+Puzzle.createColourPalette(2);
+Puzzle.addColourable('circle');
