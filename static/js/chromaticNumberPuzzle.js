8e301a364189d07a9c5fe5723f8194efb7adf462
Peter Collingridge <peter.collingridge@gmail.com>
2019-05-19 19:15:29 +0100

    Add new exercises

--- static/js/chromaticNumberPuzzle.js
+++ static/js/chromaticNumberPuzzle.js
@@ -1,11 +1,6 @@
 // JSON for chromatic number puzzle definitions
 var puzzles = [
     {
-        colours: 2,
-        scale: 24,
-        nodes: [[-2, -1], [0, -1], [2, -1], [2, 1], [0, 1], [-2, 1]],
-        edges: getLoopOfEdges(6),
-    }, {
         colours: 1,
         nodes: [[0, 0]],
     }, {
@@ -41,6 +36,21 @@ var puzzles = [
         scale: 24,
         nodes: [[-2, -1], [0, -1], [2, -1], [2, 1], [0, 1], [-2, 1]],
         edges: getLoopOfEdges(6),
+    }, {
+        colours: 2,
+        scale: 24,
+        nodes: [[-2, -1], [0, -1], [2, -1], [2, 1], [0, 1], [-2, 1]],
+        edges: getLoopOfEdges(6).concat([[1, 4]]),
+    }, {
+        colours: 3,
+        scale: 24,
+        nodes: [[-2, -1], [0, -1], [2, -1], [2, 1], [0, 1], [-2, 1]],
+        edges: getLoopOfEdges(6).concat([[0, 4], [1, 4], [2, 4]]),
+    }, {
+        colours: 3,
+        scale: 24,
+        nodes: [[-2, -1], [0, -1], [2, -1], [2, 1], [0, 1], [-2, 1]],
+        edges: getLoopOfEdges(6).concat([[0, 4], [1, 4], [1, 3]]),
     }
 ];
 
