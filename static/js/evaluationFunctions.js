8dbc18ce1f9c30fe8a8653589ddd358a02e54273
Peter Collingridge <peter.collingridge@gmail.com>
2019-05-21 22:27:40 +0100

    Create basic repeating patterns page

--- static/js/evaluationFunctions.js
+++ static/js/evaluationFunctions.js
@@ -1,4 +1,5 @@
 function areAllItemsColoured(items) {
+    console.log(items)
     for (var i = 0; i < items.length; i++) {
         if (items[i].colour === undefined) {
             return false;
@@ -17,8 +18,26 @@ function areAllEdgeNodesDifferent(edges) {
     return true;
 }
 
+function doNodeColoursMatch(nodes1, nodes2) {
+    if (nodes1.length !== nodes2.length) { return false; }
+    console.log(nodes1)
+    console.log(nodes2)
+    for (var i = 0; i < nodes1.length; i++) {
+        if (nodes1[i].colour !== nodes2[i].colour) {
+            return false;
+        }
+    }
+    return true;
+}
+
 // Function for determining where the puzzle has been solved
 var evaluateChromaticGraph = function () {
     return areAllItemsColoured(this.graph.nodes) &&
         areAllEdgeNodesDifferent(this.graph.edges);
+}
+
+// Function for determining where the puzzle has been solved
+var evaluateRepeatingPattern = function () {
+    return areAllItemsColoured(this.answerNodes) &&
+        doNodeColoursMatch(this.answer, this.answerNodes);
 }
--- static/js/puzzleLoaders.js
+++ static/js/puzzleLoaders.js
@@ -6,3 +6,36 @@ var chromaticGraphLoader = function(data) {
     data.scale = data.scale || 32;
     this.graph = addGraph(this, data);
 };
+
+// Function for loading puzzle data into Puzzle object
+var repeatingPatternloader = function(data) {
+    this.toolbar.createColourPalette(data.colours);
+    this.answer = data.answer.map(function(item) {
+        return { colour: item };
+    });
+
+    var scale = data.scale || 32;
+    var r = data.nodeRadius || 8;
+    var n = data.pattern.length + data.answer.length;
+    var x = -((n - 1) * scale) / 2;
+
+    this.answerNodes = [];
+    for (var i = 0; i < n; i++) {
+        var nodeObject = {
+            cx: x,
+            cy: 0,
+            r: r
+        };
+
+        if (i < data.pattern.length) {
+            nodeObject.class = 'colour-' + data.pattern[i];
+            addCircle(this.element, nodeObject);
+        } else {
+            var nodeElement = addCircle(this.element, nodeObject);
+            makeElementColourable(nodeElement, nodeObject, this);
+            this.answerNodes.push(nodeObject);
+        }
+
+        x += scale
+    }
+};
