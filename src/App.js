48e0b2c8b9ab6851235cd8c77105ae8b765a0407
Peter Collingridge <peter.collingridge@gmail.com>
2019-05-28 20:42:28 +0100

    Get 404 handling working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import PuzzleMap from './PuzzleMap';
 import ColourGraph1 from './Puzzles/ColourGraphs1';
@@ -12,7 +12,9 @@ import './Icons/icons.css';
 
 export default () =>
     <Router>
-        <Route path="/colour-graphs-1" component={ColourGraph1} />
-        <Route path="/colour-graphs-2" component={ColourGraph2} />
-        <Route path="/" exact component={PuzzleMap} />
+        <Switch>
+            <Route path="/colour-graphs-1" component={ColourGraph1} />
+            <Route path="/colour-graphs-2" component={ColourGraph2} />
+            <Route path="/" component={PuzzleMap} />
+        </Switch>
     </Router>
