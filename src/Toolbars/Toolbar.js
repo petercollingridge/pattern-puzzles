0e25b0ece1018adc9b2c9d8817fa203908b0c488
Peter Collingridge <peter.collingridge@nanoporetech.com>
2019-05-30 12:39:22 +0100

    Make more accessible by adding keyboard input for toolbar

--- src/Toolbars/Toolbar.js
+++ src/Toolbars/Toolbar.js
@@ -4,6 +4,12 @@ import React from 'react';
 const TOOLBAR_R = 112;
 const DELTA_ANGLE = Math.PI / 20;
 
+const handleKeyDown = (evt, callback) => {
+    if (evt.key === 'Enter' || evt.keyCode === 32) {
+        callback();
+    }
+};
+
 export default function ({ puzzle, nColours, selectedColour }) {
     const r = 6;
     let angle = Math.PI - DELTA_ANGLE * (nColours - 1) / 2;
@@ -12,19 +18,26 @@ export default function ({ puzzle, nColours, selectedColour }) {
     for (let i = 1; i <= nColours; i++) {
         const cx = (TOOLBAR_R + r) * Math.cos(angle);
         const cy = (TOOLBAR_R + r) * Math.sin(angle);
+        
         let className = `colour-palette colour-${i}`;
         if (selectedColour === i) {
             className += ' selected';
         }
 
+        const setColour = () => puzzle.setState({ selectedColour: i });
+
         colours.push(
             <circle
                 className={className}
+                ariaRole="button"
+                ariaSelected={selectedColour === i}
+                tabIndex="0"
                 key={i}
                 cx={cx}
                 cy={cy}
                 r={r}
-                onClick={() => puzzle.setState({ selectedColour: i})}
+                onClick={setColour}
+                onKeyDown={(evt) => handleKeyDown(evt, setColour) }
             />
         );
 
@@ -38,7 +51,7 @@ export default function ({ puzzle, nColours, selectedColour }) {
 
     return <g className="toolbar">
         <circle className={selectColourIndicator} r="109" />
-        <g className="colour-pallete">
+        <g className="colour-palette">
             { colours }
         </g>
     </g>;
