c49d68f9dd437bf597c52587c38c73a706671666
Peter Collingridge <peter.collingridge@gmail.com>
2019-06-03 21:22:55 +0100

    Create glow effect and rotate toolbar on long devices

--- src/Puzzles/PuzzlePage.js
+++ src/Puzzles/PuzzlePage.js
@@ -98,10 +98,17 @@ export default class PuzzlePage extends React.Component {
 
             <svg id="puzzle-chamber" viewBox="-128 -128 256 256" preserveAspectRatio="xMidYMid slice">
                 <defs>
-                    <filter id="shadow-filter" x="0" y="0" width="120%" height="120%">
+                    <filter id="shadow-filter">
                         <feOffset result="offOut" in="SourceAlpha" dx="2" dy="2"/>
                         <feGaussianBlur result="blurOut" in="offOut" stdDeviation="5" />
                         <feBlend in="SourceGraphic" in2="blurOut" mode="normal" />
+                    </filter>
+                    <filter id="glow-filter" x="-200%" y="-200%" width="400%" height="400%">
+                        <feGaussianBlur stdDeviation="1" result="colouredBlur" />
+                        <feMerge>
+                            <feMergeNode in="coloredBlur"/>
+                            <feMergeNode in="SourceGraphic"/>
+                        </feMerge>
                     </filter>
                 </defs>
 
