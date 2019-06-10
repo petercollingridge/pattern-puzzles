3124479fe909be61efad193d90c7f9c0b3985ed8
Peter Collingridge <peter.collingridge@gmail.com>
2019-06-10 22:33:17 +0100

    Create category display

--- src/Puzzles/Categorisation.js
+++ src/Puzzles/Categorisation.js
@@ -9,36 +9,46 @@ import { allItemsColoured, sequencesMatch } from '../utils/evaluation';
 
 const puzzle1 = [
 	{
-		colourPalette: 1,
+		colourPalette: 2,
         categories: [
             [getGraphObject({ nodes: [[0, 0, 1]] })],
             [getGraphObject({ nodes: [[0, 0, 2]] })],
-            [getGraphObject({ nodes: [[0, 0, 1]] })],
+            // [getGraphObject({ nodes: [[0, 0, 1]] })],
+            // [getGraphObject({ nodes: [[0, 0, 1]] })],
         ]
     }
 ];
 
-const getCircleOffsets = (n) => n;
-
-const displayCategories = (puzzle, selectedColour, update) => {
-    const R = 96;
-    const n = puzzle.length;
-    const theta =  Math.PI * 2 / n;
-
+const getCirclePackedInCircle = (R, n) => {
+    const dAngle = Math.PI * 2 / n;
     const phi = Math.PI * (0.5 - 1 / n);
     const cPhi = Math.cos(phi);
     const d = R / (1 + cPhi);
     const r = d * cPhi;
 
-    const items = [];
+    const circles = [];
     for (let i = 0; i < n; i++) {
-        const x = d * Math.sin(i * theta);
-        const y = d * Math.cos(i * theta);
-        items.push(<circle fill="#eee" key={i} cx={x} cy={y} r={r} />)
+        circles.push([
+            d * Math.sin((i - 0.5) * dAngle),
+            d * Math.cos((i - 0.5) * dAngle),
+            r
+        ]);
     }
 
+    return circles;
+}
+
+const displayCategories = (puzzle, selectedColour, update) => {
+    const outerR = 100;
+    const categories = getCirclePackedInCircle(outerR, puzzle.length);
+
     return <g>
-        { items }
+        { categories.map((category, i) =>
+        <g key={i} transform={`translate(${ category[0] } ${ category[1] })`}>
+                <circle className="colourable category" r={category[2]} />
+                <Graph {...puzzle[i].item} />
+            </g>)
+        }
     </g>
 }
 
