c7da9d6406abd07e1701955316b17ca94c0ca66c
Peter Collingridge <peter.collingridge@gmail.com>
2019-06-15 10:46:19 +0100

    Add role to button

--- src/Toolbars/Button.js
+++ src/Toolbars/Button.js
@@ -4,8 +4,8 @@ import React from 'react';
 export default (props) => {
     let className = props.className || "";
     if (props.flashing) { className += " flashing"; }
-    
+
     return <svg viewBox="-12 -12 24 24">
-        <circle className={className} r="9" onClick={props.onClick} />
+        <circle className={className} r="9" onClick={props.onClick} role="button" />
     </svg>
 }
