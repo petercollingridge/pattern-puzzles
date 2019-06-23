fa70bd4ef9b9befdefc8f4a151914ffa4b3cc21b
Peter Collingridge <peter.collingridge@gmail.com>
2019-06-23 17:16:22 +0100

    Create sequences 2 puzzle

--- src/PuzzleMap/Icons/Sequences.js
+++ src/PuzzleMap/Icons/Sequences.js
@@ -10,4 +10,19 @@ const SequencesIcon1 = () =>
         <rect className="colour-1" x="22.5" y="-7.5" width="15" height="15" rx="2" ry="2" />
     </g>
 
-export default [SequencesIcon1];
+const SequencesIcon2 = () => 
+    <g className="graph-nodes">
+        {[1, 2, 1, 0, 1, 2, 1].map((colour, index) =>
+            <rect
+                key={index}
+                className={"colour-" + colour}
+                x={-42 + index * 12}
+                y="-7.5"
+                width="12" height="12" rx="2" ry="2" />
+        )}
+    </g>
+
+export default [
+    SequencesIcon1,
+    SequencesIcon2
+];
--- src/Puzzles/Sequences.js
+++ src/Puzzles/Sequences.js
@@ -46,7 +46,43 @@ const puzzles1 = [
     }
 ];
 
-const puzzles = [puzzles1];
+const puzzles2 = [
+    {
+        colourPalette: 1,
+        pattern: [1, 1, 0, 1, 1],
+        answer: [1]
+    }, {
+        colourPalette: 2,
+        pattern: [1, 2, 1, 0, 1, 2, 1],
+        answer: [2]
+    }, {
+        colourPalette: 3,
+        pattern: [1, 2, 3, 0, 2, 3],
+        answer: [1]
+    }, {
+        colourPalette: 2,
+        pattern: [1, 1, 2, 2, 1, 0, 0, 2],
+        answer: [1, 2]
+    }, {
+        colourPalette: 2,
+        pattern: [1, 2, 2, 1, 2, 0, 0, 2, 2],
+        answer: [2, 1]
+    }, {
+        colourPalette: 3,
+        pattern: [1, 2, 3, 2, 1, 0, 1, 2, 3, 2, 1],
+        answer: [3]
+    }, {
+        colourPalette: 3,
+        pattern: [1, 2, 3, 0, 0, 3, 1, 2, 3, 2, 1],
+        answer: [2, 1]
+    }, {
+        colourPalette: 3,
+        pattern: [1, 1, 0, 0, 0, 1, 2, 3, 1, 1, 2, 3],
+        answer: [2, 3, 1]
+    }
+];
+
+const puzzles = [puzzles1, puzzles2];
 
 const evaluate = ({ sequence, target }) =>
     allItemsColoured(sequence) &&
