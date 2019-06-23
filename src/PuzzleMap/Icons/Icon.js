b9bb1e2b57f79efdfe541b86697fc29a6b5f3624
Peter Collingridge <peter.collingridge@gmail.com>
2019-06-23 11:16:29 +0100

    Improve focus of puzzle icons

--- src/PuzzleMap/Icons/Icon.js
+++ src/PuzzleMap/Icons/Icon.js
@@ -2,7 +2,17 @@ import React from 'react';
 import './icons.css';
 
 export default (props) =>
-    <svg viewBox="-50 -50 100 100">
+    <svg viewBox="-55 -55 110 110">
+        <defs>
+            <filter id="glow-spotlight" x="-200%" y="-200%" width="400%" height="400%">
+                <feGaussianBlur stdDeviation="2" result="colouredBlur" />
+                <feMerge>
+                    <feMergeNode in="coloredBlur"/>
+                    <feMergeNode in="SourceGraphic"/>
+                </feMerge>
+            </filter>
+        </defs>
         <circle className="spotlight" cx="0" cy="0" r="49" />
+        <circle className="spotlight-outline" cx="0" cy="0" r="49" />
         { props.children }
     </svg>
