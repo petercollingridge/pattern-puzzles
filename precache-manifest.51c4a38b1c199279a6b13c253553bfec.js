3e460015e3ccd61d347c4551c182be1dd416c0d9
Peter Collingridge <peter.collingridge@gmail.com>
2019-06-25 21:27:33 +0100

    Updates

--- precache-manifest.51c4a38b1c199279a6b13c253553bfec.js
+++ precache-manifest.51c4a38b1c199279a6b13c253553bfec.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "01ebc02c8aa257b0162e339b28a7c0e5",
+    "revision": "32c2c5a2c0a553887fb980eb736ac9b6",
     "url": "/pattern-puzzles/index.html"
   },
   {
-    "revision": "86b2ea55f1f20cf86086",
+    "revision": "c50b62d2a20ac0bdf740",
     "url": "/pattern-puzzles/static/css/main.bf9fc643.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pattern-puzzles/static/js/2.ddced5a7.chunk.js"
   },
   {
-    "revision": "86b2ea55f1f20cf86086",
-    "url": "/pattern-puzzles/static/js/main.aff031b0.chunk.js"
+    "revision": "c50b62d2a20ac0bdf740",
+    "url": "/pattern-puzzles/static/js/main.090a24ae.chunk.js"
   },
   {
     "revision": "e30b49739cd196546000",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pattern-puzzles/precache-manifest.6ad82ef5c737627ec80fc75ed90a40e4.js"
+  "/pattern-puzzles/precache-manifest.51c4a38b1c199279a6b13c253553bfec.js"
 );
 
 self.addEventListener('message', (event) => {
