0ca4f7bb182628dd9898ed7a3a200393bc03a0e2
Peter Collingridge <peter.collingridge@gmail.com>
2019-06-27 23:09:42 +0100

    Create icon for new sequence puzzle

--- src/PuzzleMap/Icons/Sequences.js
+++ src/PuzzleMap/Icons/Sequences.js
@@ -22,8 +22,39 @@ const SequencesIcon2 = () =>
         )}
     </g>
 
+const SequencesIcon3 = () => <React.Fragment>
+    <defs>
+        <g id="triangle" className="graph-edges">
+            <line x1="-15" y1="13" x2="0" y2="-13" />
+            <line x1="-15" y1="13" x2="15" y2="13" />
+            <line x1="0" y1="-13" x2="15" y2="13" />
+        </g>
+        <g id="triangle-1">
+            <use href="#triangle" />
+            <g className="graph-nodes">
+                <circle className="colour-1" cx="-15" cy="13" r="7" />
+                <circle className="colour-2" cx="0" cy="-13" r="7" />
+                <circle className="colour-1" cx="15" cy="13" r="7" />
+            </g>
+        </g>
+        <g id="triangle-2">
+            <use href="#triangle" />
+            <g className="graph-nodes">
+                <circle className="colour-2" cx="-15" cy="13" r="7" />
+                <circle className="colour-1" cx="0" cy="-13" r="7" />
+                <circle className="colour-2" cx="15" cy="13" r="7" />
+            </g>
+        </g>
+    </defs>
+
+    <use href="#triangle-1" transform="translate(-34, -4) scale(0.45)"/>
+    <use href="#triangle-2" transform="translate(-11, -4) scale(0.45)"/>
+    <use href="#triangle-1" transform="translate(11, -4) scale(0.45)"/>
+    <use href="#triangle-2" transform="translate(34, -4) scale(0.45)"/>
+</React.Fragment>
+
 export default [
     SequencesIcon1,
     SequencesIcon2,
-    SequencesIcon2,
+    SequencesIcon3,
 ];
