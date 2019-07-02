850374f7914a22ebce7b9d81de2aa13ad2ec61ec
Peter Collingridge <peter.collingridge@gmail.com>
2019-07-02 22:35:30 +0100

    Fix button styling

--- src/Toolbars/Button.js
+++ src/Toolbars/Button.js
@@ -6,13 +6,21 @@ export default (props) => {
     let className = props.className || "";
     if (props.flashing) { className += " flashing"; }
 
-    return <svg viewBox="-12 -12 24 24">
+    let buttonProps = {};
+    if (!props.link) {
+        buttonProps.tabIndex = 0;
+    }
+    
+    if (props.onClick) {
+        buttonProps.onClick = props.onClick;
+        buttonProps.onKeyDown = (evt) => handleKeyDown(evt, props.onClick);
+        buttonProps.role = "button";
+    }
+
+    return <svg viewBox="-15 -15 31 31">
         <circle
-            className={className}
-            tabIndex="0"
             r="9"
-            onClick={props.onClick}
-            onKeyDown={(evt) => handleKeyDown(evt, props.onClick)}
-            role="button" />
+            className={className}
+            {...buttonProps} />
     </svg>
 };
