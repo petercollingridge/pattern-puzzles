7da520581a840e95e47ed0072fe2514958e596ac
Peter Collingridge <peter.collingridge@gmail.com>
2019-07-19 23:06:54 +0100

    Fix isColourable when item is fixed

--- src/Puzzles/PuzzleComponents/utils.js
+++ src/Puzzles/PuzzleComponents/utils.js
@@ -3,20 +3,19 @@ import { handleKeyDown } from '../../utils/common';
 
 // Given a node, return props to make it colourable
 export function isColourable(item, i, colourItem, className="") {
-    if (item.fixed || !colourItem) {
-        return {
-            className: `fill-${item.colour} ${ className }`
-        };
-    }
-    
-    const colourThisItem = () => colourItem(i);
-
-    className += " colourable ";
     if (item.colour) {
-        className += `fill-${item.colour}`;
+        className += ` fill-${item.colour}`;
     } else {
-        className += "no-fill";
+        className += " no-fill";
     }
+
+    if (item.fixed || !colourItem) {
+        return { className };
+    }
+
+    className += " colourable";
+
+    const colourThisItem = () => colourItem(i);
 
     return {
         className,
