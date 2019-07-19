af8800a7d6650b0ce79fb49da70b4c7ff7457613
Peter Collingridge <peter.collingridge@nanoporetech.com>
2019-07-19 12:43:06 +0100

    Make sure Line component gets className

--- src/Puzzles/PuzzleComponents/Primitives.js
+++ src/Puzzles/PuzzleComponents/Primitives.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
 
-export const Line = ({ x1, y1, x2, y2 }) =>
-    <line x1={x1} y1={y1} x2={x2} y2={y2} />
+export const Line = ({ className, x1, y1, x2, y2 }) =>
+    <line className={className} x1={x1} y1={y1} x2={x2} y2={y2} />
