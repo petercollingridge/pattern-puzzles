6f93ee1974746d85cceff909646dc3da516d5c70
Peter Collingridge <peter.collingridge@nanoporetech.com>
2019-07-19 12:44:09 +0100

    Add more Hamiltonian puzzles

--- src/Puzzles/GraphTheory.js
+++ src/Puzzles/GraphTheory.js
@@ -7,14 +7,24 @@ import React from 'react';
 import PuzzlePage from './PuzzlePage';
 import { getGraphObject } from './puzzleLoaders';
 import { ColourablePath } from './PuzzleComponents/Graph';
+
 import { allItemsColoured } from '../utils/evaluation';
+import { getPointsOnACircle, getLoopOfEdges, getLineOfEdges } from '../utils/graphUtils';
 
 
 const hamiltonianPath = [
 	{
-        colourPalette: 1,
-        nodes: [[-1, 0], [0, 0], [1, 0]],
-        edges: [[0, 1], [1, 2]]
+        nodes: getPointsOnACircle(4),
+        edges: getLoopOfEdges(4)
+    }, {
+        nodes: getPointsOnACircle(6),
+        edges: getLoopOfEdges(6)
+    }, {
+        nodes: getPointsOnACircle(4),
+        edges: getLoopOfEdges(4).concat([[1, 3]])
+    }, {
+        nodes: getPointsOnACircle(4),
+        edges: [[0, 3], [2, 3], [0, 2], [1, 3]]
     }
 ];
 
@@ -22,6 +32,7 @@ const puzzles = [hamiltonianPath];
 
 export const GraphTheory = (n) =>
     <PuzzlePage
+        colourPalette={1}
         puzzles={puzzles[n]}
         evaluate={({ nodes }) => allItemsColoured(nodes)}
 		getPuzzleObject={getGraphObject}
