4402daabac79b41952ebcc33d13bb7732b83cfab
Peter Collingridge <peter.collingridge@gmail.com>
2019-07-20 21:49:48 +0100

    Update Hamiltonian puzzle

--- src/Puzzles/GraphTheory.js
+++ src/Puzzles/GraphTheory.js
@@ -9,27 +9,27 @@ import { getGraphObject } from './puzzleLoaders';
 import { ColourablePath } from './PuzzleComponents/Graph';
 
 import { allItemsColoured } from '../utils/evaluation';
-import { getPointsOnACircle, getLoopOfEdges } from '../utils/graphUtils';
+import { getPointsOnACircle, getLoopOfEdges, getLineOfEdges } from '../utils/graphUtils';
 
 
 const hamiltonianPath = [
 	{
-        nodes: getPointsOnACircle(4),
-        edges: getLoopOfEdges(4)
-    }, {
         nodes: getPointsOnACircle(6),
         edges: getLoopOfEdges(6)
     }, {
-        nodes: getPointsOnACircle(4),
-        edges: getLoopOfEdges(4).concat([[1, 3]])
+        nodes: getPointsOnACircle(6),
+        edges: getLoopOfEdges(6).concat([[1, 4]])
     }, {
-        nodes: getPointsOnACircle(4),
-        edges: [[0, 3], [2, 3], [0, 2], [1, 3]]
+        nodes: getPointsOnACircle(6),
+        edges: getLineOfEdges(6)
     }, {
-        nodes: getPointsOnACircle(4, { r: 2 }).concat([[Math.SQRT2, 0]]),
-        edges: [[0, 1], [2, 3], [0, 3], [1, 3], [2, 4]]
+        nodes: getPointsOnACircle(6),
+        edges: getLineOfEdges(6).concat([[2, 5]])
     }, {
-        nodes: getPointsOnACircle(3).concat(getPointsOnACircle(3, { r: 2.5 })),
+        nodes: getPointsOnACircle(6),
+        edges: [[2, 3], [3, 4], [4, 5], [5, 0], [1, 4], [0, 3]]
+    }, {
+        nodes: getPointsOnACircle(3, { r: 0.75 }).concat(getPointsOnACircle(3, { r: 2 })),
         edges: getLoopOfEdges(3).concat(getLoopOfEdges(3, 5)).concat([[2, 5]])
     }
 ];
