08bb3db8e27430c19d4214efc3f43b3834e444ee
Peter Collingridge <peter.collingridge@gmail.com>
2019-08-07 22:26:55 +0100

    Add more dominating set questions

--- src/Puzzles/GraphTheory.js
+++ src/Puzzles/GraphTheory.js
@@ -10,11 +10,12 @@ import { ColourablePath, DominatingSet } from './PuzzleComponents/Graph';
 
 import { allItemsColoured } from '../utils/evaluation';
 import {
-    getPointsOnACircle, 
+    getNodesOnCircle,
     getLoopOfEdges,
     getLineOfEdges,
     linearGraph,
     loopGraph,
+    spokeGraph,
 } from '../utils/graphUtils';
 
 
@@ -22,33 +23,33 @@ const R3 = Math.sqrt(3);
 const hamiltonianPath = [
     loopGraph(6),
     {
-        nodes: getPointsOnACircle(6),
+        nodes: getNodesOnCircle(6),
         edges: getLoopOfEdges(6).concat([[1, 4]])
     }, {
-        nodes: getPointsOnACircle(6),
+        nodes: getNodesOnCircle(6),
         edges: getLineOfEdges(6)
     }, {
-        nodes: getPointsOnACircle(6),
+        nodes: getNodesOnCircle(6),
         edges: getLineOfEdges(6).concat([[2, 5]])
     }, {
-        nodes: getPointsOnACircle(6),
+        nodes: getNodesOnCircle(6),
         edges: [[2, 3], [3, 4], [4, 5], [5, 0], [1, 4], [0, 3]]
     }, {
-        nodes: getPointsOnACircle(3, { r: 0.75 })
-                .concat(getPointsOnACircle(3, { r: 2 })),
+        nodes: getNodesOnCircle(3, { r: 0.75 })
+                .concat(getNodesOnCircle(3, { r: 2 })),
         edges: getLoopOfEdges(3)
                 .concat(getLoopOfEdges(3, 5))
                 .concat([[2, 5]])
     }, {
-        nodes: getPointsOnACircle(3, { r: 0.75 })
-                .concat(getPointsOnACircle(3, { r: 2 }))
+        nodes: getNodesOnCircle(3, { r: 0.75 })
+                .concat(getNodesOnCircle(3, { r: 2 }))
                 .concat([[-R3, -2], [R3, -2]]),
         edges: getLoopOfEdges(3)
                 .concat(getLoopOfEdges(3, 5))
                 .concat([[0, 3], [2, 5], [3, 6], [4, 7]])
     }, {
-        nodes: getPointsOnACircle(3, { r: 0.75 })
-                .concat(getPointsOnACircle(3, { r: 2 }))
+        nodes: getNodesOnCircle(3, { r: 0.75 })
+                .concat(getNodesOnCircle(3, { r: 2 }))
                 .concat([[-R3, -2], [R3, -2]]),
         edges: getLoopOfEdges(3)
                 .concat(getLoopOfEdges(3, 5))
@@ -59,7 +60,30 @@ const hamiltonianPath = [
 const dominatingSet = [
     {
         maxColours: 1,
-        graph: linearGraph(3)
+        graph: linearGraph(3, { scale: 2, r: 10 })
+    }, {
+        maxColours: 1,
+        graph: spokeGraph(5, { scale: 2, r: 10 })
+    }, {
+        maxColours: 2,
+        graph: loopGraph(4, { scale: 2, r: 10 })
+    }, {
+        maxColours: 2,
+        graph: {
+            nodes: getNodesOnCircle(6, { r: 2 }).concat([[0, 0, 0]]),
+            edges: getLoopOfEdges(6).concat([[6, 0], [6 , 1], [6, 3], [6, 4]]),
+            r: 10
+        }
+    }, {
+        maxColours: 3,
+        graph: {
+            nodes: getNodesOnCircle(5, { r: 3 }).concat(getNodesOnCircle(5, { r: 1.5 })),
+            edges: getLoopOfEdges(5).concat([
+                [0, 5], [1, 6], [2, 7], [3, 8], [4, 9],
+                [5, 7], [6, 8], [7, 9], [5, 8], [6, 9]
+            ]),
+            r: 10
+        }
     }
 ];
 
