c855a7a31ba2eac7a7aaa5f94e0ea47f43a7d9b8
Peter Collingridge <peter.collingridge@nanoporetech.com>
2021-01-30 12:04:16 +0000

    Add combination puzzle

--- src/PuzzleMap/Icons/Combinatorics.js
+++ src/PuzzleMap/Icons/Combinatorics.js
@@ -22,4 +22,42 @@ const Combinatorics1 = () => <g>
     </g>
 </g>
 
-export default [Combinatorics1]
+const Combinatorics2 = () => <g>
+    <g transform="translate(0, -24)">
+        <line className="graph-edges" x1="-20" x2="20" />
+        <g className="graph-nodes">
+            <circle className="colour-1" cx="-20" r="6" />
+            <circle className="colour-2" cx="0" r="6" />
+            <circle className="colour-2" cx="20" r="6" />
+        </g>
+    </g>
+    <g transform="translate(0, -8)">
+        <line className="graph-edges" x1="-20" x2="20" />
+        <g className="graph-nodes">
+            <circle className="colour-2" cx="-20" r="6" />
+            <circle className="colour-1" cx="0" r="6" />
+            <circle className="colour-2" cx="20" r="6" />
+        </g>
+    </g>
+    <g transform="translate(0, 8)">
+        <line className="graph-edges" x1="-20" x2="20" />
+        <g className="graph-nodes">
+            <circle className="colour-2" cx="-20" r="6" />
+            <circle className="colour-2" cx="0" r="6" />
+            <circle className="colour-1" cx="20" r="6" />
+        </g>
+    </g>
+    <g transform="translate(0, 24)">
+        <line className="graph-edges" x1="-20" x2="20" />
+        <g className="graph-nodes">
+            <circle className="colour-1" cx="-20" r="6" />
+            <circle className="colour-2" cx="0" r="6" />
+            <circle className="colour-1" cx="20" r="6" />
+        </g>
+    </g>
+</g>
+
+export default [
+    Combinatorics1,
+    Combinatorics2,
+];
