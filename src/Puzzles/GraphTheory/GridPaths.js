96505919855d643f2d1ccae3dc8fdcfb78be1da3
Peter Collingridge <peter.collingridge@nanoporetech.com>
2021-02-16 16:56:24 +0000

    Add more grid graph puzzles

--- src/Puzzles/GraphTheory/GridPaths.js
+++ src/Puzzles/GraphTheory/GridPaths.js
@@ -58,6 +58,26 @@ const puzzles2 = [{
     graph: gridGraph(3, 3),
     start: 0,
     end: 6,
+}, {
+    graph: gridGraph(4, 3),
+    start: 0,
+    end: 3,
+}, {
+    graph: gridGraph(4, 3),
+    start: 0,
+    end: 9,
+}, {
+    graph: gridGraph(4, 3),
+    start: 0,
+    end: 11,
+}, {
+    graph: gridGraph(4, 3),
+    start: 0,
+    end: 7,
+}, {
+    graph: gridGraph(4, 3),
+    start: 3,
+    end: 8,
 }];
 
 const puzzles = [puzzles1, puzzles2];
@@ -69,8 +89,33 @@ const getGraph = ({ graph, start, end }) => {
     graphObject.nodes[start].fixed = true;
     graphObject.nodes[start].current = true;
     graphObject.nodes[end].colour = 2;
-    graphObject.nodes[end].fixed = true;
+    graphObject.end = graphObject.nodes[end];
+    graphObject.end.colour = 2;
+    graphObject.end.fixed = true;
+
     return graphObject;
+}
+
+const evaluate = (graph) => {
+    if (allItemsColoured(graph.nodes)) {
+        // Add final path segment
+        const finalIndex = graph.path[graph.path.length - 1].index;
+        graph.end.edges[finalIndex].colour = 1;
+
+        // Hide non-coloured ends
+        graph.edges.forEach(edge => {
+            if (!edge.colour) {
+                edge.hidden = true;
+            }
+        });
+        return true;
+    } else {
+        // Remove colour of final path segment
+        Object.values(graph.end.edges).forEach(edge => edge.colour = 0);
+
+        // Make sure all edges are showing
+        graph.edges.forEach(edge => edge.hidden = false);
+    }
 }
 
 const GridPath = (n) =>
@@ -78,7 +123,7 @@ const GridPath = (n) =>
         colourPalette={1}
         clearButton
         puzzles={puzzles[n]}
-        evaluate={({ nodes }) => allItemsColoured(nodes)}
+        evaluate={evaluate}
         getPuzzleObject={getGraph}
         displayPuzzle={ColourableHamilitonianPath}
     />
