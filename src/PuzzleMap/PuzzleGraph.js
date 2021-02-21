3c1c87f7dc60c895ad52a8aa5edb80ffad405c06
Peter Collingridge <peter.collingridge@nanoporetech.com>
2021-02-21 15:27:27 +0000

    Add draggable puzzle graph

--- src/PuzzleMap/PuzzleGraph.js
+++ src/PuzzleMap/PuzzleGraph.js
@@ -1,24 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Icon from './Icons/Icon';
 import { Link } from "react-router-dom";
 import PuzzleData from "../Puzzles/allPuzzles";
 
-import './puzzleMap.css';
+import './PuzzleGraph.css';
 
 
 function Graph() {
+    const [dragging, setDragging] = useState(false);
+    const [lastPosition, setLastPosition] = useState(false);
+    const [offset, setOffset] = useState({ x: 0, y: 0 });
+
+    function onMouseDown(evt) {
+        setDragging(true);
+        setLastPosition({ x: evt.pageX, y: evt.pageY });
+        evt.stopPropagation();
+        evt.preventDefault();
+    }
+
+    function onMouseMove(evt) {
+        if (!dragging) { return; }
+        const x = offset.x + evt.pageX - lastPosition.x;
+        const y = offset.y + evt.pageY - lastPosition.y;
+        setLastPosition({ x: evt.pageX, y: evt.pageY });
+        setOffset({ x, y });
+        evt.stopPropagation();
+        evt.preventDefault();
+    };
+
+    function onMouseUp(evt) {
+        setDragging(false);
+        evt.stopPropagation();
+        evt.preventDefault();
+    }
+
     return (
-        <main className="puzzle-graph">
-            { PuzzleData.map(({ icon, slug }) => {
-                return (
-                    <div className="map-link puzzle-node" key={slug}>
-                        <Link to={slug}>
-                            <Icon>{ icon }</Icon>
-                        </Link>
-                    </div>
-                );
-            }) }
+        <main onMouseUp={onMouseUp} onMouseMove={onMouseMove} onMouseDown={onMouseDown}>
+            <nav className="puzzle-graph">
+                { PuzzleData.map(({ available, icon, slug, x, y }) => {
+                    const className = `map-link puzzle-node ${available ? '' : 'inactive'}`;
+                    const style = { left: x + offset.x, top: y + offset.y };
+                    
+                    return (
+                        <div key={slug} className={className} style={style}>
+                            <Link to={slug}>
+                                <Icon>{ icon }</Icon>
+                            </Link>
+                        </div>
+                    );
+                }) }
+            </nav>
+
+            <svg id="shadow-ring" viewBox="-128 -128 256 256" preserveAspectRatio="xMidYMid slice">
+                <defs>
+                    <filter id="shadow">
+                        <feGaussianBlur in="SourceGraphic" stdDeviation="15" />
+                    </filter>
+                </defs>
+
+                <circle r="200" stroke="black" fill="none" strokeWidth="240" filter="url(#shadow)"/>
+                <circle r="300" stroke="black" fill="none" strokeWidth="240" />
+            </svg>
         </main>
     );
 }
